d163c76ec8bfcd42fefb826e2f026b3f33698436
lea-vpetrova <lea.p.vk@gmail.com>
2023-10-14 15:12:28 +0300

    adding more completed exercises

--- JS_fundamentals/Ex/Ex_Functions/palindromeInt.js
+++ JS_fundamentals/Ex/Ex_Functions/palindromeInt.js
@@ -1,3 +1,9 @@
 function palindromInt(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i].toString();
+        let revNum = num.split('').reverse().join('');
+        let result = Number(num)==Number(revNum)?"true":"false";
+        console.log(result);
+    }
 }
+palindromInt([123,323,421,121]);
