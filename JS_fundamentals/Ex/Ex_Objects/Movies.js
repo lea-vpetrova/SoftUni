495c3d2cc41223f8d2f4e255ca75e54e92c1be18
lea-vpetrova <lea.p.vk@gmail.com>
2023-10-31 18:08:22 +0200

    more ex

---
+++ JS_fundamentals/Ex/Ex_Objects/Movies.js
@@ -0,0 +1,44 @@
+function movies(input) {
+    let obj = [];
+
+    input.forEach(line => {
+        if (line.includes('addMovie')) {
+            let name = line.split('addMovie ')[1];
+            obj.push({ name });
+        }
+        else if (line.includes('directedBy')) {
+            let [name, director] = line.split(' directedBy ');
+
+            let movie = obj.find(m => m.name === name);
+            if (movie) {
+                movie.director = director;
+            }
+        }
+        else if (line.includes('onDate')) {
+            let [name, date] = line.split(' onDate ');
+
+            let movie = obj.find(m => m.name === name);
+
+            if (movie) {
+                movie.date = date;
+            }
+        }
+    });
+
+    obj.forEach(movie => {
+        if (movie.name && movie.director && movie.date) {
+            console.log(JSON.stringify(movie));
+        }
+    });
+}
+
+movies([
+    'addMovie Fast and Furious',
+    'addMovie Godfather',
+    'Inception directedBy Christopher Nolan',
+    'Godfather directedBy Francis Ford Coppola',
+    'Godfather onDate 29.07.2018',
+    'Fast and Furious onDate 30.07.2018',
+    'Batman onDate 01.08.2018',
+    'Fast and Furious directedBy Rob Cohen'
+]);
