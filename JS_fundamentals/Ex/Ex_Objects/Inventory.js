84a451960e376ad2c21b99dea26ae11cb9746cef
lea-vpetrova <lea.p.vk@gmail.com>
2023-11-13 15:18:09 +0200

    obj ex

--- JS_fundamentals/Ex/Ex_Objects/Inventory.js
+++ JS_fundamentals/Ex/Ex_Objects/Inventory.js
@@ -1,11 +1,17 @@
 function inventory(input) {
+    let heroRegister = {};
+    let heros =[];
     for (const record of input) {
         let [name,level, items] = record.split('/');
-        let heroRegister ={ name : name, 
+        heroRegister ={ name : name, 
                         level: Number(level),
                         items: items };
-        console.log(`Hero: ${heroRegister.name}\nlevel => ${heroRegister.level}\nItems => ${heroRegister.items}`);
+        heros.push(heroRegister);
     }
+    heros.sort(function(a,b){return a.level - b.level;});
+        //console.log(`Hero: ${heroRegister.name}\nlevel => ${heroRegister.level}\nItems => ${heroRegister.items}`);
+        return heros;
+    
 }
 inventory([ 'Isacc / 25 / Apple, GravityGun', 'Derek / 12 / BarrelVest, DestructionSword', 'Hes / 1 / Desolator, Sentinel, Antara']);
 //inventory([ 'Batman / 2 / Banana, Gun', 'Superman / 18 / Sword', 'Poppy / 28 / Sentinel, Antara']);
