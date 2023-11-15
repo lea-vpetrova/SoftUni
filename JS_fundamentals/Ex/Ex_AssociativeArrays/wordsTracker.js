c6d7ed6827b2d035c195212285995d1100d50d2f
lea-vpetrova <lea.p.vk@gmail.com>
2023-11-15 13:08:00 +0200

    ex

--- JS_fundamentals/Ex/Ex_AssociativeArrays/wordsTracker.js
+++ JS_fundamentals/Ex/Ex_AssociativeArrays/wordsTracker.js
@@ -1,16 +1,27 @@
 function ocurrence(input) {
-    let findOccurence = input.shift().split(' ');
+    let searchedWords = input.shift().split(' ');
     let wordCounts = {};
-    for (let line of input) {
-        let words = line.split(', ');
-        for (let word of words) {
-            if (wordCounts.hasOwnProperty(findOccurence)) {
-                wordCounts[findOccurence]++;
-            }
-            else {
-                wordCounts[findOccurence] = 1;
-            }
+    for (let word of searchedWords) {
+        wordCounts[word] = 0;
+    }
+    for (let word of input) {
+        if (word in wordCounts) {
+            wordCounts[word]++;
         }
     }
 
+    let entries = Object.entries(wordCounts).sort((a,b) => b[1] -a[1]);
+    for(let [word, rep] of entries)
+    {
+        console.log(`${word} - ${rep}`);
+    }
 }
+ocurrence([
+
+    'is the',
+
+    'first', 'sentence', 'Here', 'is',
+
+    'another', 'the', 'And', 'finally', 'the',
+
+    'the', 'sentence']);
