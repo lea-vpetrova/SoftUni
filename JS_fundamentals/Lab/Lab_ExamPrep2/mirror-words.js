59064bccebe4ebaba508898bae6b3fe14749c092
lea-vpetrova <lea.p.vk@gmail.com>
2023-12-01 23:14:10 +0200

    ex2

---
+++ JS_fundamentals/Lab/Lab_ExamPrep2/mirror-words.js
@@ -0,0 +1,38 @@
+    //compose regex
+    //find all matches in a loop
+    //for every match:
+    // - check if second word reversed is the same as the first word
+
+    //print result
+function mirrorWords([input]) {
+    let pattern = /(#|@)([A-Za-z]{3,})\1\1([A-Za-z]{3,})\1/gi;
+    let result = [];
+    let count = 0;
+    let match = pattern.exec(input);
+    while(match){
+        count++;
+
+        let first = match[2];
+        let second = match[3];
+
+        if(first == second.split('').reverse().join(''))
+        {
+            result.push(`${first} <=> ${second}`)
+        }
+
+        match = pattern.exec(input);
+    }
+    if (count == 0) {
+        console.log('No word pairs found!');
+    }else{
+        console.log(`${count} word pairs found!`);
+    }
+
+    if (result.length == 0) {
+        console.log('No mirror words!');
+    }else{
+        console.log('The mirror words are:');
+        console.log(result.join(', '));
+    }
+}
+mirrorWords(['@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r']);
