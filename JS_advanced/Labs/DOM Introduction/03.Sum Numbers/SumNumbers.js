3dfd56794d872c97c6bbe1f660818d76433219ce
lea-vpetrova <lea.p.vk@gmail.com>
2024-02-05 19:22:36 +0200

    dom ex3

--- JS_advanced/Labs/DOM Introduction/03.Sum Numbers/SumNumbers.js
+++ JS_advanced/Labs/DOM Introduction/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,6 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    let n1 = document.getElementById('num1').value;
+    let n2 = document.getElementById('num2').value;
+    let sum = Number(n1)+Number(n2);
+    document.getElementById('sum').value = sum;
 }
