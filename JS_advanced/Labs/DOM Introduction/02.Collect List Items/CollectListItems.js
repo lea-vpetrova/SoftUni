c67fdc4b7d5cad38590b51819f24e2a294ce983c
lea-vpetrova <lea.p.vk@gmail.com>
2024-02-05 19:18:10 +0200

    dom ex2

--- JS_advanced/Labs/DOM Introduction/02.Collect List Items/CollectListItems.js
+++ JS_advanced/Labs/DOM Introduction/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,7 @@
 function extractText() {
-    // TODO
+    let items = document.querySelectorAll("ul#items li");
+    let textarea = document.querySelector("#result");
+    for (let node of items) {
+        textarea.value +=node.textContent+"\n";
+    }
 }
