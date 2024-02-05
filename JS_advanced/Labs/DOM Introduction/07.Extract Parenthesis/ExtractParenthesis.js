bb13b8be87b472f974042d448e8146c5311d60b3
lea-vpetrova <lea.p.vk@gmail.com>
2024-02-05 19:47:18 +0200

    dom ex7

--- JS_advanced/Labs/DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS_advanced/Labs/DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,12 @@
-function extract(content) {
+function extract(elementId) {
+    let p = document.getElementById(elementId).textContent;
+    let pattern = /\(([^)]+)\)/g;
+    let result = [];
 
+    let match = pattern.exec(p);
+    while(match){
+        result.push(match[1]);
+        match = pattern.exec(p);
+    }
+    return result.join('; ');
 }
