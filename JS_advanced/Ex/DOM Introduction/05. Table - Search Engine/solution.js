2642e8a5b7df224b212519b7cd3efc5230168ff6
lea-vpetrova <lea.p.vk@gmail.com>
2024-02-06 21:19:19 +0200

    dom intro ex5

--- JS_advanced/Ex/DOM Introduction/05. Table - Search Engine/solution.js
+++ JS_advanced/Ex/DOM Introduction/05. Table - Search Engine/solution.js
@@ -1,8 +1,21 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
-
+   let searchingInput = document.getElementById("searchField");
+   let tableRows = Array.from(document.querySelectorAll("tbody tr"))
    function onClick() {
-      //   TODO:
-
+      let text = searchingInput.value;
+      for (let row of tableRows) {
+         let tableCols = Array.from(row.querySelectorAll("td"));
+         for (let col of tableCols) {
+            if (col.textContent.includes(text)) {
+               row.classList.add("select");
+               break;
+            }
+            else{
+               row.classList.remove("select")
+            }
+         }
+      }
+      searchingInput.value = "";
    }
 }
