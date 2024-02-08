6a5afa561b4ae49d7dc8532c6f141d4d985c05c9
lea-vpetrova <lea.p.vk@gmail.com>
2024-02-09 00:48:29 +0200

    dom2 ex5

--- JS_advanced/Labs/DOM Manipulations and Events/05. Highlight-Active/app.js
+++ JS_advanced/Labs/DOM Manipulations and Events/05. Highlight-Active/app.js
@@ -1,3 +1,14 @@
 function focused() {
-    console.log('TODO:...');
+    let divInputList = Array.from(document.querySelectorAll("div input"));
+    divInputList.forEach(div => div.addEventListener("focus", focus));
+    divInputList.forEach(div => div.addEventListener("blur", focusLost));
+
+    function focus(event) {
+        let parent = event.target.parentElement;
+        parent.classList.add("focused")
+    }
+    function focusLost(event) {
+        let parent = event.target.parentElement;
+        parent.classList.remove("focused")
+    }
 }
