26533b3e9cecc3c4323373509de7fb92a202a4e7
lea-vpetrova <lea.p.vk@gmail.com>
2024-02-08 11:39:05 +0200

    dom pt2 ex1

--- JS_advanced/Labs/DOM Manipulations and Events/01. List-Of-Items/app.js
+++ JS_advanced/Labs/DOM Manipulations and Events/01. List-Of-Items/app.js
@@ -1,3 +1,9 @@
 function addItem() {
-    console.log('TODO:...');
+    let text = document.getElementById("newItemText").value;
+    let li = document.createElement("li");
+    let list = document.createTextNode(text);
+    li.appendChild(list);
+    document.getElementById("items").appendChild(li);
+
+    document.getElementById("newItemText").value = "";
 }
