057aad603d6e886a86374923fb3dc5d96fd2c7c0
lea-vpetrova <lea.p.vk@gmail.com>
2024-02-08 19:42:57 +0200

    dom2 ex2

--- JS_advanced/Labs/DOM Manipulations and Events/02. Delete-From-Table/app.js
+++ JS_advanced/Labs/DOM Manipulations and Events/02. Delete-From-Table/app.js
@@ -1,3 +1,19 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let table = Array.from(document.querySelectorAll("tbody tr"));
+    let inputField = document.querySelector('[name="email"]');
+    let resultField = document.getElementById("result");
+    let success = false;
+    for (let row of table) {
+        let emailField = row.children[1].textContent;
+        if (emailField == inputField.value) {
+            row.remove();
+            success = true;
+        }
+    }
+    if (success) {
+        resultField.textContent = "Deleted.";
+    }else{
+        resultField.textContent = "Not found.";
+    }
+    document.querySelector('[name="email"]').value ="";
 }
