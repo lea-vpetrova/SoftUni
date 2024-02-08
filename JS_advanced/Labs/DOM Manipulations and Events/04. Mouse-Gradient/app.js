10d54de6bbc7a557285f64936b96acef39aa17f4
lea-vpetrova <lea.p.vk@gmail.com>
2024-02-09 00:28:23 +0200

    dom2 ex4

--- JS_advanced/Labs/DOM Manipulations and Events/04. Mouse-Gradient/app.js
+++ JS_advanced/Labs/DOM Manipulations and Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,16 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradient = document.getElementById("gradient");
+    gradient.addEventListener("mousemove", gradientMove);
+    gradient.addEventListener("mouseout", gradientOut);
+
+    function gradientMove(event) {
+        let boxWidth = event.target.clientWidth;
+        let mousePosition = event.offsetX / (boxWidth - 1);
+        let perc = Math.trunc(mousePosition*100);
+        document.getElementById("result").textContent = `${perc}%`;
+    }
+
+    function gradientOut() {
+        document.getElementById("result").textContent = "";
+    }
 }
