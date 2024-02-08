96ac0abcf38ef04312b663c9e310ec6e7671b8a4
lea-vpetrova <lea.p.vk@gmail.com>
2024-02-09 01:03:16 +0200

    dom2 ex6

--- JS_advanced/Labs/DOM Manipulations and Events/06. Dynamic-Validation/app.js
+++ JS_advanced/Labs/DOM Manipulations and Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    console.log('TODO:...');
+    let inputElem = document.getElementById("email");
+    inputElem.addEventListener('change', checkEmail);
+
+    function checkEmail(event) {
+        let regex = /^([\w\-.]+)@([a-z]+)(\.[a-z]+)+$/;
+        if (regex.test(event.target.value)) {
+            event.target.removeAttribute('class');
+            return;
+        }
+        event.target.className = 'error';
+        
+    }
 }
