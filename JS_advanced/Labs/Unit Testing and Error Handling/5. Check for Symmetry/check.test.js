6be3e2de0be1077e9af288f66b98503ea03c916c
lea-vpetrova <lea.p.vk@gmail.com>
2024-02-16 00:44:15 +0200

    unit test

---
+++ JS_advanced/Labs/Unit Testing and Error Handling/5. Check for Symmetry/check.test.js
@@ -0,0 +1,20 @@
+import { expect } from "chai";
+import { isSymmetric } from "./check.js";
+
+describe(`check if array is symmetric`, () => {
+    it(`input -> [0,0] -> true`, () => {
+        expect(isSymmetric([1, 1])).to.equal(true)
+    })
+    it(`input -> [0,1] -> false`, () => {
+        expect(isSymmetric([0, 1])).to.equal(false)
+    })
+    it(`input -> 'a' -> false`, () => {
+        expect(isSymmetric('')).to.equal(false)
+    })
+    it(`input -> [] -> true`, () => {
+        expect(isSymmetric([1, 1, 1])).to.equal(true)
+    })
+    it(`input -> [1, '1'] -> false`, () => {
+        expect(isSymmetric([1, '1'])).to.equal(false)
+    })
+});
