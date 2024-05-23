5810797375853a0166e4e5270b8c56d3f3fd20dd
Lea Petrova <116258035+lea-vpetrova@users.noreply.github.com>
2024-05-23 13:59:47 +0300

    dom intro ex

--- JS_advanced/Ex/DOM Introduction/08. Generate Report/report.js
+++ JS_advanced/Ex/DOM Introduction/08. Generate Report/report.js
@@ -1,3 +1,16 @@
 function generateReport() {
-    //TODO
+    const checkBoxes = document.querySelectorAll("table>thead>tr>th>input");
+    const rows = document.querySelectorAll("tbody>tr");
+    let output = [];
+    for (let i = 0; i < rows.length; i++) {
+        let obj ={};
+        let values = Array.from(rows[i].getElementsByTagName("td")).map(el=> el.textContent);
+        for (let j = 0; j < checkBoxes.length; j++) {
+            if (checkBoxes[j].checked) {
+                obj[checkBoxes[j].name]=values[j];
+            }
+        }
+        output.push(obj);
+    }
+    document.querySelector("#output").value= JSON.stringify(output,null,2);
 }
--- JS_advanced/Ex/DOM Introduction/09. Number Convertor/solution.js
+++ JS_advanced/Ex/DOM Introduction/09. Number Convertor/solution.js
@@ -1,4 +1,34 @@
 function solve() {
+    const inputFieldEl = document.getElementById("input");
+    const selectTo = document.getElementById("selectMenuTo");
 
-    //TODO...
+    const options = [
+        {value: 'binary', text: 'Binary'},
+        {value: 'hexadecimal', text: 'Hexadesimal'}
+    ];
+
+    options.forEach(
+        el => {
+            const opt = document.createElement('option');
+            opt.value = el.value;
+            opt.textContent = el.text;
+            selectTo.appendChild(opt);
+        }
+    );
+    const resultFieldEl = document.getElementById("result");
+    const convertBtn = document.querySelector("button");
+
+    convertBtn.addEventListener('click', onClick);
+
+    function onClick(){
+        let number = Number(inputFieldEl.value);
+        const convertTo = selectTo.value;
+
+        if (convertTo === 'binary') {
+            resultFieldEl.value = number.toString(2);
+        }
+        else if (convertTo === 'hexadecimal') {
+            resultFieldEl.value = number.toString(16).toUpperCase();
+        }
+    }
 }
