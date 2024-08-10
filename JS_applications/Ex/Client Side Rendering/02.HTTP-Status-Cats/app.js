0c67de84bdb4978aa0a9f3c0957c9bb916ef29fa
Lea Petrova <116258035+lea-vpetrova@users.noreply.github.com>
2024-08-10 12:51:08 +0300

    http stat cats

---
+++ JS_applications/Ex/Client Side Rendering/02.HTTP-Status-Cats/app.js
@@ -0,0 +1,50 @@
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+import { cats } from "./catSeeder.js";
+
+const section = document.querySelector("#allCats");
+
+const template = html`
+    <ul>
+        ${cats.map(cat => createCatCard(cat))}
+    <ul>`;
+
+function createCatCard(cat) {
+
+    const catTemplate = html`
+        <li>
+            <img src="images/${cat.imageLocation}.jpg " width="250" height="250" alt="Card image cap">
+            <div class="info">
+                <button class="showBtn" @click=${showDetails}>Show status code</button>
+                <div class="status" style="display: none" id=${cat.id}>
+                    <h4>Status Code: ${cat.statusCode}</h4>
+                    <p>${cat.statusMessage}</p>
+                </div>
+            </div>
+        </li>`;
+    
+    return catTemplate;
+}
+
+render(template, section);
+
+function showDetails(event) {
+    
+    event.preventDefault();
+
+    const button = event.target;
+    const parent = event.target.parentElement;
+    const details = parent.querySelector('.status');
+
+    if (details.style.display === 'block') {
+
+        details.style.display = 'none';
+        button.textContent = 'Show status code';
+        
+    }
+    else{
+
+        details.style.display = 'block';
+        button.textContent = 'Hide status code';
+
+    }
+}
